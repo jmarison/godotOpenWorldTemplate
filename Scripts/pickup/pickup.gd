323c4b06520020b35c1a9418b3f94d60e2255ae0
Jake Marison <msmarisonj@gmail.com>
2025-03-30 13:59:13 -0500

    added item bob

--- Scripts/pickup/pickup.gd
+++ Scripts/pickup/pickup.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 @export var item:Item
+var time : float = 0.0  
+var speed = 2.0
+var amplitude : float = 0.00075
 
 func _ready():
 	var instance = item.scene.instantiate()
@@ -12,4 +15,6 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		queue_free()
 
 func _process(delta: float) -> void:
+	time += delta  
+	position.y += sin(time * speed) * amplitude  
 	rotate_y(0.008)
