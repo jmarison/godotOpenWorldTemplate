4b231db9187e644e7b246532cdfee1eb67aede99
Jake Marison <msmarisonj@gmail.com>
2025-05-27 12:49:39 -0500

    items now properly display as a stack within the crafting menu

--- Scripts/Crafting/itemGrid/item_grid.gd
+++ Scripts/Crafting/itemGrid/item_grid.gd
@@ -3,15 +3,30 @@ extends GridContainer
 
 @export var slot_scene:PackedScene
 var blankItem:Item = preload("res://DATA/Items/blank.tres")
+var shownItems: Array[Item] = []
 
 func craftingdisplay(items:Array[Item]):
 	for child in get_children():
 		child.queue_free()
 	
+	shownItems.clear()
+	
 	for item in items:
+		var  found = false
+		for existingStack in shownItems:
+			if existingStack.name == item.name:
+				existingStack.count += 1
+				found = true
+				break
+		if not found:
+			var newItem = item.duplicate()
+			newItem.count = 1
+			shownItems.append(newItem)
+	
+	for item in shownItems:
 		var slot = slot_scene.instantiate()
 		add_child(slot)
-		slot.craftingDisplay(item)
+		slot.display(item)
 
 func display(items:Array[Item]):
 	var num_slots = 60
